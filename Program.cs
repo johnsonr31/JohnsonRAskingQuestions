66306ede561702d25db20201aa75e2436f562ca3
ricRobo4 <artistr3@proton.me>
2022-10-18 11:56:04 -0700

    started writing the while loop

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Richard Johnson
+// 10-18-2022
+
+// Here is where we declare the "playAgain" bool
+bool playAgain = true;
+
+// This while loop will continue looping as long as playAgain is true
+while(playAgain == true)
+{
+    Console.WriteLine("Would you like me to ask you some questions?");
+    Console.Write("YES or NO: ");
+    
+}
