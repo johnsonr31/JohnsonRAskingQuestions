080cbf7afdc62ea245c9490e02f5a1f926b69d0f
ricRobo4 <artistr3@proton.me>
2022-10-20 14:22:21 -0700

    removed minutes, as they didn't work right

--- Program.cs
+++ Program.cs
@@ -5,12 +5,10 @@
 bool playAgain = true;
 bool isNumber;
 bool isNumber2;
+bool time;
 int wakeTime = 0;
-int wakeMinute = 0;
-int hourLengthMax = 1;
+int hourLengthMax = 2;
 int inputHourLength;
-int inputMinuteLength;
-int minuteLengthMax = 2;
 string digits = "1234567890";
 
 // This while loop will continue looping as long as playAgain is true
@@ -34,53 +32,44 @@ while(playAgain == true)
         Console.WriteLine("Good.");
         Console.WriteLine("What is your name?");
         Console.Write("Name: ");
-        // The user will be asked to input their name. Their input will be stored as a string and converted to be all uppercase
+        // The user will be asked to input their name. Their input will then be stored as a string to be printed later
         string name = Console.ReadLine();
-        name = name.ToUpper();
+        // name = name.ToUpper();
 
         Console.WriteLine(" ");
         Console.WriteLine("Now, what time did you wake up today?");
-        Console.Write("Enter the hour you woke up (use 1 digit): ");
+        time = true;
+        while(time == true)
+        {
+            //
+        Console.Write("Enter the hour you woke up (Standard Time): ");
         string awake = Console.ReadLine();
         // After the user's input is taken, the code below will validate that their input is a number, and convert it to an int
         isNumber = Int32.TryParse(awake, out wakeTime);
         inputHourLength = awake.Length;
 
-        if(isNumber == true && inputHourLength == hourLengthMax) 
+        if(isNumber == true && inputHourLength <= hourLengthMax) 
         {
-            Console.WriteLine(" ");
-            Console.Write("Now enter the minutes of the time you woke up (use 2 digits): ");
-            string minutes = Console.ReadLine();
-            isNumber2 = Int32.TryParse(minutes, out wakeMinute);
-            inputMinuteLength = minutes.Length;
-                if(isNumber2 == true && inputMinuteLength == minuteLengthMax)
-                {
-                    // This will run if the number is valid
-                    Console.WriteLine($"{wakeTime}:{wakeMinute} AM or PM?");
-                    Console.Write("Enter AM or PM: ");
-                    string amPM = Console.ReadLine();
-                    amPM = amPM.ToUpper();
+        // This will run if the number is valid
+        Console.WriteLine($"{wakeTime} AM or PM?");
+        Console.Write("Enter AM or PM: ");
+        string amPM = Console.ReadLine();
+        amPM = amPM.ToUpper();
 
-                        if (amPM == "AM" || amPM == "PM")
-                        {
-                        // This will run if the player enters AM or PM
-                        Console.WriteLine($"So your name is {name}, and you woke up at {wakeTime}:{wakeMinute} {amPM}.");
-                        Console.WriteLine("Hopefully you got enough sleep.");
-                        Console.WriteLine(" ");
-                        }
-                        else
-                        {
-                        // This will run if the player enters anything else
-                        Console.WriteLine("Invalid input");
-                        Console.WriteLine(" ");
-                        }
-                }
-                else
-                {
-                    // This will run if the user entered something that isn't a number
-                    Console.WriteLine("Invalid Input");
-                    Console.WriteLine(" ");
-                }
+            if (amPM == "AM" || amPM == "PM")
+            {
+                // This will run if the player enters AM or PM
+                Console.WriteLine($"So your name is {name}, and you woke up at {wakeTime} {amPM}.");
+                Console.WriteLine("Hopefully you got enough sleep.");
+                Console.WriteLine(" ");
+            }
+            else
+            {
+                // This will run if the player enters anything else
+                Console.WriteLine("Invalid input");
+                Console.WriteLine(" ");
+            }
+                
 
 
             
@@ -91,6 +80,7 @@ while(playAgain == true)
             Console.WriteLine("Invalid Input");
             Console.WriteLine(" ");
             
+        }
         }
         
 
