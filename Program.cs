6a923b88b1a115171ca0d7a15218e8fc5b0e102b
ricRobo4 <artistr3@proton.me>
2022-10-20 14:53:55 -0700

    added tryparse to name input

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ bool playAgain = true;
 bool isNumber;
 bool isNumber2;
 bool time;
+bool nameNum = true;
+int num = 0;
 int wakeTime = 0;
 int hourLengthMax = 2;
 int inputHourLength;
@@ -33,56 +35,72 @@ while(playAgain == true)
         Console.WriteLine("What is your name?");
         Console.Write("Name: ");
         // The user will be asked to input their name. Their input will then be stored as a string to be printed later
+        
+        while(nameNum == true)
+        {
         string name = Console.ReadLine();
         // name = name.ToUpper();
+        isNumber = Int32.TryParse(name, out num);
 
-        Console.WriteLine(" ");
-        Console.WriteLine("Now, what time did you wake up today?");
-        time = true;
-        while(time == true)
-        {
-            //
-        Console.Write("Enter the hour you woke up (Standard Time): ");
-        string awake = Console.ReadLine();
-        // After the user's input is taken, the code below will validate that their input is a number, and convert it to an int
-        isNumber = Int32.TryParse(awake, out wakeTime);
-        inputHourLength = awake.Length;
-
-        if(isNumber == true && inputHourLength <= hourLengthMax) 
-        {
-        // If the user's input is a valid number, their number will be printed, and they will be asked to input AM or PM
-        Console.WriteLine($"{wakeTime} AM or PM?");
-        Console.Write("Enter AM or PM: ");
-        string amPM = Console.ReadLine();
-        amPM = amPM.ToUpper();
-
-            if (amPM == "AM" || amPM == "PM")
+            if(isNumber != true)
             {
-                // If the user enters AM or PM, the program will print the user's input to the console
-                Console.WriteLine($"So your name is {name}, and you woke up at {wakeTime} {amPM}.");
-                Console.WriteLine("Hopefully you got enough sleep.");
+                nameNum = false;
                 Console.WriteLine(" ");
-                time = false;
-            }
-            else
-            {
-                // If the player enters anything else, the loop will start again
-                Console.WriteLine("Invalid input");
-                Console.WriteLine(" ");
-            }
+                Console.WriteLine("Now, what time did you wake up today?");
+                time = true;
+                while(time == true)
+                {
+                Console.Write("Enter the hour you woke up (Standard Time): ");
+                string awake = Console.ReadLine();
+                // After the user's input is taken, the code below will validate that their input is a number, and convert it to an int
+                isNumber = Int32.TryParse(awake, out wakeTime);
+                inputHourLength = awake.Length;
+
+                if(isNumber == true && inputHourLength <= hourLengthMax) 
+                {
+                // If the user's input is a valid number, their number will be printed, and they will be asked to input AM or PM
+                Console.WriteLine($"{wakeTime} AM or PM?");
+                Console.Write("Enter AM or PM: ");
+                string amPM = Console.ReadLine();
+                amPM = amPM.ToUpper();
+
+                if (amPM == "AM" || amPM == "PM")
+                {
+                    // If the user enters AM or PM, the program will print the user's input to the console
+                    Console.WriteLine($"So your name is {name}, and you woke up at {wakeTime} {amPM}.");
+                    Console.WriteLine("Hopefully you got enough sleep.");
+                    Console.WriteLine(" ");
+                    time = false;
+                }
+                else
+                {
+                    // If the player enters anything else, the loop will start again
+                    Console.WriteLine("Invalid input");
+                    Console.WriteLine(" ");
+                }
                 
 
 
             
-        }
-        else
-        {
-            // This will run if the user inputs anything that isn't a valid number
-            Console.WriteLine("Invalid Input");
-            Console.WriteLine(" ");
+                }
+                else
+                {
+                    // This will run if the user inputs anything that isn't a valid number
+                    Console.WriteLine("Invalid Input");
+                    Console.WriteLine(" ");
             
+                }
+                }
+            }
+            else
+            {
+                Console.WriteLine("That is a number.");
+                Console.Write("Please Enter a name: ");
+            }
         }
-        }
+            
+
+        
         
 
     }
